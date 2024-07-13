59e87b1d372150da9199a4e839f8e0b3ef4095b7
masato0720 <masato1904@masato1904.onmicrosoft.com>
2024-07-13 13:14:04 +0900

    課題ｍｄｎの追加

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,6 @@
+//今日の日付の取得
+const today = new Date();
+const year = today.getFullYear();
+const month = today.getMonth()+1;
+const date = today.getDate();
+console.log(year+"年" + month+"月" + date+"日");
